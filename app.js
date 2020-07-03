5baf24bc5d54364d589ddac24520f716ed40e85c
root <webdevbootcamp100>
2020-07-03 05:58:08 +0000

    add start script to package.json file

---
+++ app.js
@@ -0,0 +1,38 @@
+const express          = require('express'),
+	  app              = express(),
+	  mongoose         = require('mongoose'),
+	  bodyParser       = require('body-parser'),
+	  methodOverride   = require('method-override'),
+	  expressSanitizer = require('express-sanitizer');
+
+//app configuration
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static('public'));      
+
+//database connection
+mongoose.connect('mongodb+srv://colt:9HXkkHt5v67u@TD@cluster0-ddvng.mongodb.net/appdb?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
+//schema setup
+const campgroundSchema = new mongoose.Schema({
+	title:String,
+	image:String,
+	body:String,
+	created:{type:Date,default:Date.now}
+});
+//model setup     
+const Blog = mongoose.model('apptable',campgroundSchema);    
+
+//RESTful routes 
+//index route 
+app.get('/',(req,res)=>{
+	res.redirect('/campgrounds');  
+});
+app.get('/campgrounds',(req,res)=>{
+	res.render('index');   
+});     
+
+
+
+
+//server connection
+app.listen(process.env.PORT,process.env.IP);  
